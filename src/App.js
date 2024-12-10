7117efc1fe23ee5de2d3b2bfcee529a865c216f9
sukhad123 <sukhadadhikari3@gmail.com>
2024-12-10 15:10:25 -0500

    next.js to react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ 
+import Navbar from "../../portfolio/src/components/navbar.js"
+import Body from "../../portfolio/src/components/body.js"
+import About from "../../portfolio/src/components/about.js"
+ 
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Body />
+      <About />
+    </main>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'sour-gummy': ['"Sour Gummy"', 'serif'],
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
