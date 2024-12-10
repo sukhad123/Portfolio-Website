857a579a8617340178c83057d75aebc454c2ac9c
sukhad123 <sukhadadhikari3@gmail.com>
2024-12-10 17:18:13 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
- 
+ import "./index.css"
 import Navbar from "./components/navbar.js"
 import Body from "./components/body.js"
 import About from "./components/about.js"
+import { motion, useScroll } from "framer-motion";
  
 function App() {
+  const { scrollYProgress } = useScroll();
   return (
+    <>
+    <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
     <main>
+      <motion.div style={{ scaleX: scrollYProgress }} />
       <Navbar />
       <Body />
       <About />
     </main>
+    </>
+  
   );
 }
 
--- src/components/body.js
+++ src/components/body.js
@@ -1,8 +1,9 @@
- 
+import { motion } from "framer-motion";
 export default function Body(){
  
 
 return<div className = "mainBody">
+       
     <div className = "content grid justify-items-center">
         <div>
          <img  className = "image"src="/images/programmer.png" alt="Logo" /> 
@@ -11,10 +12,19 @@ return<div className = "mainBody">
         <p className ="text-center pSmall  ">My name is Sukhad, I am a aspiring Software Developer</p>
         <div className = "flex justify-center  ">
         <a href="https://www.linkedin.com/in/sukhad-adhikari/" target="_blank" rel="noopener noreferrer">
-        <button    type="button" className=" customBtn focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">LinkedIn</button>
+         
+        <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 300, damping: 12 }}
+        className=" customBtn focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">LinkedIn</motion.button>
         </a>
         <a href="https://github.com/sukhad123?tab=overview&from=2023-12-01&to=2023-12-31" target="_blank" rel="noopener noreferrer">
-             <button type="button" className="customBtn text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">GitHub</button>
+        <motion.button
+        whileHover={{ scale: 2}}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 300, damping: 12 }}
+        className="customBtn text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">GitHub</motion.button>
              </a>
         </div>
     </div>
