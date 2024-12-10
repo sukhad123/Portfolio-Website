2d172d8867b45709d7dc97ec68b97ec5cb4a88ba
sukhad123 <sukhadadhikari3@gmail.com>
2024-12-10 00:23:52 -0500

    update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Navbar from "./components/navbar.js"
 import Body from "./components/body.js"
 import About from "./components/about.js"
 export default function page() {
-  return<div>
+  return<div className="font-sour-gummy">
+
     <Navbar/>
     <Body/>
     <About/>
